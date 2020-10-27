3214985e991ede47d9a484e035883a6dcefbf199
Kyle Joseph Timajo <timajokyle24@gmail.com>
2020-10-27 21:04:51 +0800

    React Redux Form Start

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,41 +1,15 @@
 import React, { Component } from 'react';
-import { Breadcrumb, BreadcrumbItem, Form, FormGroup, FormFeedback, Label, Col, Input, Button } from 'reactstrap';
+import { Breadcrumb, BreadcrumbItem, Label, Col, Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors } from 'react-redux-form'
 
 class Contact extends Component{
     constructor(props) {
         super(props);
-
-        this.state = {
-            firstname: '',
-            lastname: '',
-            telnum: '',
-            email: '',
-            agree: false,
-            contactType: 'Tel.',
-            message: '',
-            touched: {
-                firstname: false,
-                lastname: false,
-                telnum: false,
-                email: false,
-            }
-        };
-
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleBlur = this.handleBlur.bind(this);
         
-    }
-
-    handleInputChange(event) {
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name = target.name;
-    
-        this.setState({
-          [name]: value
-        });
     }
 
     handleSubmit(event) {
@@ -43,46 +17,7 @@ class Contact extends Component{
         alert('Current State is: ' + JSON.stringify(this.state));
         event.preventDefault();
     }
-    handleBlur = (field) => (evt) =>{
-        this.setState({
-            touched: {
-                ...this.state.touched, [field]: true,
-            }
-        });
-    }
-    validate(firstname, lastname, telnum, email){
-        const errors = {
-            firstname: '',
-            lastname: '',
-            telnum: '',
-            email: '',
-        }
-
-        if(this.state.touched.firstname && firstname.length < 3){
-            errors.firstname = "First Name shoud be >= 3 characters";
-        }
-        else if(this.state.touched.firstname && firstname.length > 10){
-            errors.firstname = "First Name shoud be <= 10 characters";
-        }
-
-        if(this.state.touched.lastname && lastname.length < 3){
-            errors.lastname = "Last Name shoud be >= 3 characters";
-        }
-        else if(this.state.touched.lastname && lastname.length > 10){
-            errors.lastname = "Last Name shoud be <= 10 characters";
-        }
-
-        const reg = /^\d+$/;
-        if(this.state.touched.telnum && !reg.test(telnum)){
-            errors.telnum = 'Tel. number should contain only numbers';
-        }
-
-        if(this.state.touched.email && email.split('').filter( x => x === '@').length !== 1){
-            errors.email = "Email should contain a @";
-        }
-
-        return errors;
-    }
+    
     render(){
         const errors = this.validate(this.state.firstname, this.state.lastname, this.state.telnum, this.state.email);
 
